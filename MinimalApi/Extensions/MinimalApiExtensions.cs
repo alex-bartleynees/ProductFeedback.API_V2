854f1ee9ee25afd551a95293a8c699993d96d061
Alexander Bartley Nees <alexbartleynees@gmail.com>
2024-02-24 14:09:11 +1300

    Add retry

--- MinimalApi/Extensions/MinimalApiExtensions.cs
+++ MinimalApi/Extensions/MinimalApiExtensions.cs
@@ -20,7 +20,7 @@ namespace MinimalApi.Extensions
         {
             var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
             var cs = builder.Configuration.GetConnectionString("SuggestionDBConnectionString");
-            builder.Services.AddDbContext<SuggestionContext>(options => options.UseNpgsql());
+            builder.Services.AddDbContext<SuggestionContext>(options => options.UseNpgsql(cs, options => options.EnableRetryOnFailure()));
             builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             builder.Services.AddScoped<ISuggestionsRepository, SuggestionsRepository>();
             builder.Services.AddScoped<IValidator<SuggestionForCreationDto>, SuggestionForCreationDtoValidator>();
